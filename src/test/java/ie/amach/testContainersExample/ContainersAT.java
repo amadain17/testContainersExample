c41c662e4211a34970c24d9d8f8fe68834b4280a
amadain17 <mfmdevine@gmail.com>
2020-06-15 12:23:47 +0100

    Increased startup timeout added logging into Container

--- src/test/java/ie/amach/testContainersExample/ContainersAT.java
+++ src/test/java/ie/amach/testContainersExample/ContainersAT.java
@@ -5,31 +5,36 @@ import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.path.json.JsonPath;
 import io.restassured.specification.RequestSpecification;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.containers.wait.strategy.Wait;
 
+import java.time.Duration;
 import java.util.Arrays;
 
 import static io.restassured.RestAssured.given;
 import static org.apache.http.HttpStatus.SC_OK;
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Slf4j
 public class ContainersAT {
-    private RequestSpecification requestSpecification;
-
     @ClassRule
     public static GenericContainer genericContainer = new GenericContainer("test-containers-example:latest")
             .withExposedPorts(8080)
-            .waitingFor(Wait.forHttp("/employees"));
-
+            .waitingFor(Wait.forHttp("/employees")
+                    .withStartupTimeout(Duration.ofSeconds(120)));
+    static Slf4jLogConsumer logConsumer = new Slf4jLogConsumer(log);
+    private RequestSpecification requestSpecification;
 
     @Before
     public void setUp() {
-        String address = String.format("http://%s:%s", genericContainer.getContainerIpAddress()
-                , genericContainer.getFirstMappedPort());
+        genericContainer.followOutput(logConsumer);
+        String address = String.format("http://%s:%s", genericContainer.getHost(),
+                genericContainer.getMappedPort(8080));
 
         requestSpecification = new RequestSpecBuilder()
                 .setRelaxedHTTPSValidation()
